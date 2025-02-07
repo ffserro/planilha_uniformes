7c59c544dfb683ae45891703c5af0219eae82c69
ffserro <ffserro@hotmail.com>
2025-02-07 16:30:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ trip = pd.read_csv('./tripulacao.csv', delimiter=';')
 st.title('Aquisição de uniformes')
 
 if 'logged' not in st.session_state:
-  nip = st.text_input('Digite o seu NIP:')
+  st.session_state['nip'] = st.text_input('Digite o seu NIP:')
   if st.button('Enviar'):
     st.session_state['logged'] = True
     st.rerun()
 
 else:
-  st.write(nip)
+  st.write(st.session_state['nip'])
