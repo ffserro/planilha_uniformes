b9a4c9111796a88426a68c29649e067895917697
ffserro <ffserro@hotmail.com>
2025-02-07 16:41:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ if 'logged' not in st.session_state:
     st.rerun()
 
 else:
-  if st.session_state['nip'] in trip.NIP.astype(str):
-    mil = trip[trip.NIP==st.session_state['nip']]
+  mil = trip[trip.NIP==st.session_state['nip']]
+  if len(mil) != 0:
     st.markdown(f'<h2>Seja bem vindo, {mil.POSTO.iloc[0]} {mil.NOME.iloc[0]}.</h2>', unsafe_allow_html=True)
     st.write('A fim de realizar um levantamento da necessidade de aquisição de uniformes OP3 e novos bonés de viagem, concita-se a todos que preencham o formulário a seguir.')
     sr.write('A indenização destes uniformes será realizada através de desconto em BP ou pagamento de GRU para Oficiais, suboficiais e sargentos; e através de CREDIFARDA para cabos e marinheiros.')
