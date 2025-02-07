904b68bbad4d6dfd653c1f00ad5d4dd6775e63c4
ffserro <ffserro@hotmail.com>
2025-02-07 16:45:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ else:
     st.markdown(f'<h2>Seja bem vindo, {mil.POSTO.iloc[0]} {mil.NOME.iloc[0]}.</h2>', unsafe_allow_html=True)
     st.write('A fim de realizar um levantamento da necessidade de aquisição de uniformes OP3 e novos bonés de viagem, concita-se a todos que preencham o formulário a seguir.')
     st.write('A indenização destes uniformes será realizada através de desconto em BP ou pagamento de GRU para Oficiais, suboficiais e sargentos; e através de CREDIFARDA para cabos e marinheiros.')
-    st.sesion_state['inicio'] = st.button('Preencher formulário')
-  elif st.sesion_state['inicio']:
+    st.session_state['inicio'] = st.button('Preencher formulário')
+  elif st.session_state['inicio']:
     st.write('formulário')
     
