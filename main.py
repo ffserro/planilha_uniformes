8eb3e5dc7c1a27ef049c7c755725a6295dcdb9b7
ffserro <ffserro@hotmail.com>
2025-02-07 16:15:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title('Aquisição de uniformes')
 
-with st.form():
+with st.form('uniformes'):
   st.text_input('Digite o seu NIP:')
+  
+  
   st.form_submit_button('Enviar')
