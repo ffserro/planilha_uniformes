21450c7b1751bb2938a0306497e115112d909f4e
ffserro <ffserro@hotmail.com>
2025-02-07 16:28:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.title('Aquisição de uniformes')
 if 'logged' not in st.session_state:
   nip = st.text_input('Digite o seu NIP:')
   if st.button('Enviar'):
-    st.sesion_state['logged'] = True
+    st.session_state['logged'] = True
 
 else:
   st.write(nip)
