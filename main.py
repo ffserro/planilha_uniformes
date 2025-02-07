faba827ae5ce55bfd2ac7d62b1a27fa82f24092a
ffserro <ffserro@hotmail.com>
2025-02-07 16:27:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 trip = pd.read_csv('./tripulacao.csv', delimiter=';')
 
-st.dataframe(trip)
-
 st.title('Aquisição de uniformes')
 
-with st.form('uniformes'):
-  st.text_input('Digite o seu NIP:')
-  
-  
-  st.form_submit_button('Enviar')
+if 'logged' not in st.session_state:
+  nip = st.text_input('Digite o seu NIP:')
+  if st.button('Enviar'):
+    st.sesion_state['logged'] = True
+
+else:
+  st.write(nip)
